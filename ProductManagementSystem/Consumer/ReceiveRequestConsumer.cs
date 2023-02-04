812bb42e673194098900d3663aea662afce0867b
BenyaminMusavi <beni97d@gmail.com>
2023-02-04 23:59:09 +0330

    Add project files.

---
+++ ProductManagementSystem/Consumer/ReceiveRequestConsumer.cs
@@ -0,0 +1,89 @@
+﻿using Contracts;
+using MassTransit;
+
+namespace ProductManagementSystem.Consumer
+{
+    public class ReceiveRequestConsumer : IConsumer<ProductRequest>
+    {
+        private readonly IRequestClient<ProductRequest> _client;
+        //private readonly ISendEndpointProvider _sendEndpointProvider;
+        //private readonly IPublishEndpoint _publishEndpoint;
+        public ReceiveRequestConsumer(IRequestClient<ProductRequest> client)
+        {
+            _client = client;
+            //_publishEndpoint = publishEndpoint;
+            //_sendEndpointProvider = sendEndpointProvider;
+        }
+        public async Task Consume(ConsumeContext<ProductRequest> context)
+        {
+
+            //var sendToUrl = new Uri($"rabbitmq://localhost/{ Constants.SendServiceQueue}");
+
+            //var endpoint = await _sendEndpointProvider.GetSendEndpoint(sendToUrl);
+
+            //await endpoint.Send<Product>(new
+            //{
+            //    Id = context.Message.Id,
+            //    ProductName = context.Message.ProductName,
+            //    DateTime = context.Message.DateTime,
+            //    Amount = context.Message.Amount,
+            //});
+            //var modelProductRequest = new ProductRequest
+            //{
+            //    Id = context.Message.Id,
+            //    ProductName = context.Message.ProductName,
+            //    DateTime = context.Message.DateTime,
+            //    Amount = context.Message.Amount,
+            //};
+            Console.WriteLine($"Vorod {context.Message.Id}");
+
+            var message =await _client.GetResponse<ResponseMessage>(new Product
+            {
+                Id = context.Message.Id,
+                ProductName = context.Message.ProductName,
+                DateTime = context.Message.DateTime,
+                Amount = context.Message.Amount,
+            });
+
+          //  return message.Message.MessageName;
+            var m = message.Message.MessageName;
+           // var m = message.Result;
+            //message.Message.MessageName
+            Console.WriteLine($"receive {context.Message.Id}      ====>  ");
+
+            var res = "";
+
+            //var response = await _client.GetResponse<ResponseMessage>(new
+            //{
+            //    context.Message.Id,
+            //    context.Message.ProductName,
+            //    context.Message.DateTime,
+            //    context.Message.Amount,
+            //});
+
+            //await context.Send(new Product
+            //{
+            //    Id = context.Message.Id,
+            //    ProductName = context.Message.ProductName,
+            //    DateTime = context.Message.DateTime,
+            //    Amount = context.Message.Amount,
+            //});
+
+            //  var d = response;
+
+            //await _sendEndpointProvider.Send
+            //{
+
+            //};
+
+            //await _publishEndpoint.Publish(new Product
+            //{
+            //    Id = context.Message.Id,
+            //    ProductName = context.Message.ProductName,
+            //    DateTime = context.Message.DateTime,
+            //    Amount = context.Message.Amount,
+            //});
+
+        }
+    }
+}
---
+++ ProductManagementSystem/Program.cs
@@ -0,0 +1,53 @@
+using Contracts;
+using MassTransit;
+using ProductManagementSystem.Consumer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+Console.WriteLine("Product Management System");
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddMassTransit(x =>
+{
+    x.AddConsumer<ReceiveRequestConsumer>().Endpoint(e =>
+    {
+        //e.Temporary = false;
+    });
+    x.SetKebabCaseEndpointNameFormatter();
+    x.UsingRabbitMq((context, cfg) =>
+    {
+        cfg.ConfigureEndpoints(context);
+       // cfg.Host(builder.Configuration.GetValue<string>("RabbitConnection"));
+        cfg.Host(new Uri("rabbitmq://localhost/"), hst =>
+        {
+            hst.Username(Constants.UserName);
+            hst.Password(Constants.Password);
+        });
+    });
+    x.AddRequestClient<ProductRequest>();
+    //x.AddRequestClient<ProductRequest>();
+});
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
